0ca150c868226fba8166d97720237787b2a64a20
anejaf <aneja.furlan@gmail.com>
2017-05-23 22:30:04 +0200

    added testing

--- CNNfinal/cnntest.py
+++ CNNfinal/cnntest.py
@@ -11,13 +11,18 @@ import lasagne
 from lasagne import layers
 from lasagne.updates import adam
 
-import bj_loss
-import deconv_3d
+from bj_loss import binary_jaccard_index
+from deconv_3d import Conv3DLayerTransposed
+
+from lasagne import init
+from lasagne import nonlinearities
 
 #load the data
 
-data = np.load('../data-160-160-24.npy')
-seg_data = np.load('../segdata-160-160-24.npy')
+#load the data
+
+data = np.load('../train-128-160-144.npy')
+seg_data = np.load('../seg1-128-160-144.npy')
 
 data = np.expand_dims(data, axis=1)
 seg_data = np.expand_dims(seg_data, axis=1)
@@ -25,34 +30,20 @@ seg_data = np.expand_dims(seg_data, axis=1)
 print(data.shape)
 print(seg_data.shape)
 
-train_data = data[:-2]
-validation_data = data[-2:]
-train_seg = seg_data[:-2]
-validation_seg = seg_data[-2:]
+train_data = data[:8]
+#validation_data = data[-2:]
+train_seg = seg_data[:8]
+#validation_seg = seg_data[-2:]
 
-train_data *= 1/np.max(train_data)
-
-#display_numpy(train_data[1][0])
-#display_numpy(train_seg[1][0])
-#print(train_data[1][0][10][50][25:75])
-#print(train_seg[1][0][10][50][25:75])
-
-for a in range(8):
-    for i in range(24):
-        for j in range(160):
-            for k in range(160):
-                if (train_seg[a][0][i][j][k]>0.5):
-                    train_seg[a][0][i][j][k] = 1
-                    #print(str(i)+" "+str(j)+" "+str(k))
-                else:
-                    train_seg[a][0][i][j][k] = 0
+train_data /= np.max(train_data)
+print(np.max(train_data))
 
 
 
 input_var = theano.tensor.tensor5()
 target_var = theano.tensor.tensor5()
 
-layer = lasagne.layers.InputLayer((None, 1, 24, 160, 160), input_var)
+layer = lasagne.layers.InputLayer((None, 1, 128, 160, 144), input_var)
 
 #start
 layer = layers.Conv3DLayer(layer, num_filters=8, filter_size=(3, 3, 3), stride=1, pad=1, W=lasagne.init.Normal(), nonlinearity=None)
@@ -99,7 +90,7 @@ layer = layers.ParametricRectifierLayer(layer, alpha=init.Constant(0))
 layer = layers.Conv3DLayer(layer, num_filters=32, filter_size=(1, 1, 1), stride=1, pad=0, W=lasagne.init.Normal(), nonlinearity=None)
 layer = layers.BatchNormLayer(layer)
 layer = layers.ParametricRectifierLayer(layer, alpha=init.Constant(0))
-#število filtrov pri dekonvoluciji?
+#stevilo filtrov pri dekonvoluciji
 layer = Conv3DLayerTransposed(layer, num_filters=32, filter_size=(2, 2, 2), stride=2, crop=0, W=lasagne.init.Normal(), nonlinearity=None)
 layer = layers.BatchNormLayer(layer)
 layer = layers.ParametricRectifierLayer(layer, alpha=init.Constant(0))
@@ -114,7 +105,7 @@ layer = layers.ParametricRectifierLayer(layer, alpha=init.Constant(0))
 layer = layers.Conv3DLayer(layer, num_filters=16, filter_size=(1, 1, 1), stride=1, pad=0, W=lasagne.init.Normal(), nonlinearity=None)
 layer = layers.BatchNormLayer(layer)
 layer = layers.ParametricRectifierLayer(layer, alpha=init.Constant(0))
-#število filtrov pri dekonvoluciji?
+#stevilo filtrov pri dekonvoluciji?
 layer = Conv3DLayerTransposed(layer, num_filters=16, filter_size=(2, 2, 2), stride=2, crop=0, W=lasagne.init.Normal(), nonlinearity=None)
 layer = layers.BatchNormLayer(layer)
 layer = layers.ParametricRectifierLayer(layer, alpha=init.Constant(0))
@@ -129,7 +120,7 @@ layer = layers.ParametricRectifierLayer(layer, alpha=init.Constant(0))
 layer = layers.Conv3DLayer(layer, num_filters=8, filter_size=(1, 1, 1), stride=1, pad=0, W=lasagne.init.Normal(), nonlinearity=None)
 layer = layers.BatchNormLayer(layer)
 layer = layers.ParametricRectifierLayer(layer, alpha=init.Constant(0))
-#število filtrov pri dekonvoluciji?
+#stevilo filtrov pri dekonvoluciji?
 layer = Conv3DLayerTransposed(layer, num_filters=8, filter_size=(2, 2, 2), stride=2, crop=0, W=lasagne.init.Normal(), nonlinearity=None)
 layer = layers.BatchNormLayer(layer)
 layer = layers.ParametricRectifierLayer(layer, alpha=init.Constant(0))
@@ -151,7 +142,7 @@ loss = binary_jaccard_index(prediction, target_var)
 loss = loss.mean() 
 
 params = lasagne.layers.get_all_params(layer, trainable=True)
-updates = lasagne.updates.adam(loss, params, learning_rate=0.00005, beta1=0.1, beta2=0.001)
+updates = lasagne.updates.adam(loss, params, learning_rate=0.0001, beta1=0.1, beta2=0.001)
 
 train_fn = theano.function([input_var, target_var], [loss, prediction], updates=updates)
 
